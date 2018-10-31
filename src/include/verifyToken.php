792889906d3c1fc855eca4ebae15b5ae269f0562
Christopher Klewin <git@jevaka.org>
2018-10-31 17:12:21 -0400

    remove unused JWT test files

--- src/include/verifyToken.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** @var \pmill\AwsCognito\CognitoClient $client */
-$client = require(__DIR__ . '/bootstrap.php');
-
-$accessToken = '';
-
-$username = $client->verifyAccessToken($accessToken);
-var_dump($username);
