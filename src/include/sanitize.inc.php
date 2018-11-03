d4a421e68d2040f59242576918617be8ffb1e9c1
Christopher Klewin <git@jevaka.org>
2018-11-03 01:03:23 -0400

    cleanup the sanitize function

--- src/include/sanitize.inc.php
+++ src/include/sanitize.inc.php
@@ -17,21 +17,20 @@ function sanitizeMovieParams($args, $required=array()) {
 	if (!empty($missing_params)) {
 		$insane = true;
 		$response['messages'][] = 'missing required parameters: ' . join(',', $missing_params);
-		return($response);
-	}
-
-	if (!empty($args['movie_id']) &&
-		$args['movie_id'] != (int)$args['movie_id']
-		) {
-		$insane = true;
-		$response['messages'][] = 'invalid value for movie_id, expected an integer';
 	}
 
 	if (!empty($args['username']) &&
 		!preg_match('/^[a-zA-Z0-9]+/', $username)
 		) {
 		$insane = true;
-		$response['messages'][] = 'invalid username, expected alphanumeric string';
+		$response['messages'][] = 'invalid username: expected alphanumeric string';
+	}
+
+	if (!empty($args['movie_id']) &&
+		!is_integer($args['movie_id'])
+		) {
+		$insane = true;
+		$response['messages'][] = 'invalid movie_id: expected integer';
 	}
 
 	if (!empty($args['title']) &&
@@ -39,7 +38,7 @@ function sanitizeMovieParams($args, $required=array()) {
 		|| strlen($args['title']) > MOVIE_CONSTRAINTS['title_length']['max']
 		)) {
 		$insane = true;
-		$response['messages'][] = 'invalid value for title, maximum characters allowed is ' . MOVIE_CONSTRAINTS['title_length']['max'];
+		$response['messages'][] = 'invalid title: max characters allowed ' . MOVIE_CONSTRAINTS['title_length']['max'];
 	}
 
 	if (!empty($args['format']) &&
@@ -50,30 +49,30 @@ function sanitizeMovieParams($args, $required=array()) {
 	}
 
 	if (!empty($args['length']) &&
-		(!is_numeric($args['length'])
+		(!is_integer($args['length'])
 		|| $args['length'] < MOVIE_CONSTRAINTS['length']['min']
 		|| $args['length'] > MOVIE_CONSTRAINTS['length']['max']
 		)) {
 		$insane = true;
-		$response['messages'][] = 'invalid value for length, expected a number between ' . MOVIE_CONSTRAINTS['length']['min'] . ' and ' . MOVIE_CONSTRAINTS['length']['max'];
+		$response['messages'][] = 'invalid value for length, expected integer between ' . MOVIE_CONSTRAINTS['length']['min'] . ' and ' . MOVIE_CONSTRAINTS['length']['max'];
 	}
 
 	if (!empty($args['release_year']) &&
-		(!is_numeric($args['release_year'])
+		(!is_integer($args['release_year'])
 		|| $args['release_year'] < MOVIE_CONSTRAINTS['release_year']['min']
 		|| $args['release_year'] > MOVIE_CONSTRAINTS['release_year']['max']
 		)) {
 		$insane = true;
-		$response['messages'][] = 'invalid value for release_year, expected a number between ' . MOVIE_CONSTRAINTS['release_year']['min'] . ' and ' . MOVIE_CONSTRAINTS['release_year']['max'];
+		$response['messages'][] = 'invalid release_year, expected integer between ' . MOVIE_CONSTRAINTS['release_year']['min'] . ' and ' . MOVIE_CONSTRAINTS['release_year']['max'];
 	}
 
 	if (!empty($args['rating']) &&
-		(!is_numeric($args['rating'])
+		(!is_integer($args['rating'])
 		|| $args['rating'] < MOVIE_CONSTRAINTS['rating']['min']
 		|| $args['rating'] > MOVIE_CONSTRAINTS['rating']['max']
 		)) {
 		$insane = true;
-		$response['messages'][] = 'invalid value for rating, expected a number between ' . MOVIE_CONSTRAINTS['rating']['min'] . ' and ' . MOVIE_CONSTRAINTS['rating']['max'];
+		$response['messages'][] = 'invalid rating, expected integer between ' . MOVIE_CONSTRAINTS['rating']['min'] . ' and ' . MOVIE_CONSTRAINTS['rating']['max'];
 	}
 
 	if (!$insane) { $response['success'] = true; }
